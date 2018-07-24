3546ae4719066611b342cb5cd73f12c456217cfb
cdeocampo2001 <sweetcaroline601@gmail.com>
2018-07-24 23:14:00 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -3,4 +3,6 @@ list_of_PSA_cohosts = ["Favs", "Lovett", "DF", "Tommy"]
 puts list_of_PSA_cohosts[1]
 
 list_of_PSA_cohosts.push("Ben Rhodes")
+puts list_of_PSA_cohosts << "Sam Power"
+
 puts list_of_PSA_cohosts
