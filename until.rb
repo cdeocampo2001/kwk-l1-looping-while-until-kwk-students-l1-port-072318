8a811ccfbc3879c6ec7a603abdf3701caa04274a
cdeocampo2001 <sweetcaroline601@gmail.com>
2018-07-24 23:01:36 +0000

    automatically backed up by learn

---
+++ until.rb
@@ -0,0 +1,5 @@
+levitation_force = 6
+until levitation_force == 10 
+  puts "Wingardium Leviosa"
+  puts ""
+end
---
+++ while.rb
@@ -0,0 +1,6 @@
+levitation_force = 6 
+while levitation_force < 10
+ puts "Wingardium Leviosa"
+ puts "Your current levitation force is #{levitation_force + 1}"
+ levitation_force +=1
+end 
